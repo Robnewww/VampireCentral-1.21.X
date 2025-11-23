f4ba67fca7b3c600b31cbffc8fecd5b81fa1b7d9
Rob <robnewww@gmail.com>
2025-11-23 17:07:14 +0000

    First commit

---
+++ src/main/java/net/robnew/vampirecentral/VampireCentralClient.java
@@ -0,0 +1,10 @@
+package net.robnew.vampirecentral;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VampireCentralClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/robnew/vampirecentral/VampireCentralDataGenerator.java
@@ -0,0 +1,11 @@
+package net.robnew.vampirecentral;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VampireCentralDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
